447ebee1092b7931cb72d227ecd87600182c9a39
Tim Sprowl <you@example.com>
2021-11-09 20:50:08 -0500

    make watch much more robust

--- run.sh
+++ run.sh
@@ -5,7 +5,13 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT")
 
-ABSOLUTE_DIR=$(readlink -f "${@: -1}")
-WATCHED+="--watch \"$ABSOLUTE_DIR\""
+OPTIONS=$(getopt --unquoted --long routes: -- "$@")
+
+WATCHED=""
+if [ ! -z "$OPTIONS" ]; then
+  # substring(1, -3); older versions don't like seeing :-3
+  ABSOLUTE_DIR=$(readlink -f "${OPTIONS:1: -3}")
+  WATCHED+="--watch $ABSOLUTE_DIR"
+fi
 
 exec node_modules/.bin/nodemon $WATCHED "$SCRIPT_DIR/src/run.js" "$@"
